bd83430168c827938f248badf613149865d91cd2
dingx170 <dingtong@seattleu.edu>
2019-05-15 00:54:12 -0700

    Deleted content related to About, FAQ, and Contact pages in HomeController

--- HW1c/Controllers/HomeController.cs
+++ HW1c/Controllers/HomeController.cs
@@ -13,23 +13,5 @@ namespace HW1c.Controllers
             return View();
         }
 
-        public ActionResult FAQ()
-        {
-            return View();
-        }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
