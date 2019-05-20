111cbe806c6528b6718f9da12339232e91894d9e
dingx170 <dingtong@seattleu.edu>
2019-05-19 19:03:30 -0700

    Updated ActionResult for different report pages

--- HW1c/Controllers/ReportController.cs
+++ HW1c/Controllers/ReportController.cs
@@ -20,5 +20,27 @@ namespace HW1c.Controllers
 
             return View(myViewModel);
         }
+
+        // GET: Test Accuracy Report
+        public ActionResult Accuracy()
+        {
+            var myViewModel = new ReportViewModel();
+
+            myViewModel.LogViewModel = new LogBackend().Index();
+            myViewModel.NumberOfUsers = 3;
+
+            return View(myViewModel);
+        }
+
+        // GET: More Detail Report
+        public ActionResult MoreDetail()
+        {
+            var myViewModel = new ReportViewModel();
+
+            myViewModel.LogViewModel = new LogBackend().Index();
+            myViewModel.NumberOfUsers = 3;
+
+            return View(myViewModel);
+        }
     }
 }
