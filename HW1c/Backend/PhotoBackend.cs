d45bfe67855e2ae1c6f1c275d15be246bb8b8dbc
fanchenjie <fanchenjie@seattleu.edu>
2019-05-23 22:16:53 -0700

    Added fake data to PhotoBackend.cs

--- HW1c/Backend/PhotoBackend.cs
+++ HW1c/Backend/PhotoBackend.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-
+using HW1c.Models;
 namespace HW1c.Backend
 {
     public class PhotoBackend
     {
+        public PhotoViewModel Index()
+        {
+            var myData = new PhotoViewModel();
+
+            // TODO:  Populate some Log Data here...
+            myData.PhotoList.Add(new PhotoModel { PhotoID = "1", PhoneID = "ABC", RecordedDateTime = DateTime.Now, Location = "Mocc" });
+            
+            return myData;
+        }
     }
 }
