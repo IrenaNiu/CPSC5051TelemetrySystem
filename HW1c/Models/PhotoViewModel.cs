6e030a0e2458651436db2d8a57d1c8bf6568d5f1
fanchenjie <fanchenjie@seattleu.edu>
2019-05-23 21:59:32 -0700

    Added PhotoViewModel.cs

---
+++ HW1c/Models/PhotoViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1c.Models
+{
+    public class PhotoViewModel
+    {
+        public List<LogModel> PhotoList { get; set; } = new List<LogModel>();
+    }
+}
