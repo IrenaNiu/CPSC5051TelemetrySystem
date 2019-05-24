c8764f428185e21854f0e7df04bf0ec211345fce
fanchenjie <fanchenjie@seattleu.edu>
2019-05-23 22:08:10 -0700

    Added attributes to PhotoModel and PhotoViewModel

--- HW1c/Models/PhotoModel.cs
+++ HW1c/Models/PhotoModel.cs
@@ -7,6 +7,29 @@ namespace HW1c.Models
 {
     public class PhotoModel
     {
-        
+        public DateTime RecordedDateTime { get; set; } = new DateTime();
+
+        // ID of the Phone as a quid
+        public string PhoneID { get; set; }
+
+        // App Version string
+        public string PhotoID { get; set; }
+
+        // Location
+        public string Location { get; set; }
+
+        public PhotoModel()
+        {
+            Initialize();
+        }
+
+        /// <summary>
+        /// Intialize
+        /// Sets default values, such as DateTime as needed by the system
+        /// </summary>
+        public void Initialize()
+        {
+            // None right now...
+        }
     }
 }
