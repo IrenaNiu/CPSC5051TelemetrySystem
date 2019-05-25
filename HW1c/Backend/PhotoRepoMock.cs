29e2c5d10e9252685e9e1cd79fe6686ab445c7b1
Yi Niu <niuyi@seattleu.edu>
2019-05-25 04:46:49 -0700

    Added read view for photo

--- HW1c/Backend/PhotoRepoMock.cs
+++ HW1c/Backend/PhotoRepoMock.cs
@@ -3,15 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HW1c.Models;
 
 namespace HW1c.Backend
 {
-    public class PhotoRepoMock : Controller
+    /// <summary>
+    /// In Memory Implementation of a Log data store
+    /// </summary>
+    public class PhotoRepoMock : IPhotoRepo
     {
-        // GET: PhotoRepoMock
-        public ActionResult Index()
+        public List<PhotoModel> dataset = new List<PhotoModel>();
+
+        /// <summary>
+        /// Constructor for Log Repository
+        /// </summary>
+        public PhotoRepoMock()
         {
-            return View();
+            // Call for Sead data to be created
+            Initialize();
+        }
+
+        /// <summary>
+        /// Return the requested log item from the data store
+        /// if not found, return null
+        /// </summary>
+        /// <param name="id">the item to find</param>
+        /// <returns>the item from the datastore, or null</returns>
+        public PhotoModel Read(String id)
+        {
+            // Get the first instance of the record
+            var myData = dataset.First(m => m.PhotoID == id);
+            return myData;
+        }
+        /// <summary>
+        /// Return all items in the data store
+        /// </summary>
+        /// <returns>a list of all the items in the data store</returns>
+        public List<PhotoModel> Index()
+        {
+            return dataset;
+        }
+
+        /// <summary>
+        /// Sets Initial Seed Data
+        /// </summary>
+        public void Initialize()
+        {
+            dataset.Add(new PhotoModel { Value = "13", Location = "A", AppVersion = "1", PhoneID = "ABC", RecordedDateTime = DateTime.Now });
+            dataset.Add(new PhotoModel { Value = "25", Location = "A", AppVersion = "2", PhoneID = "MNO", RecordedDateTime = DateTime.Parse("01/23/2019") });
+       
         }
     }
 }
