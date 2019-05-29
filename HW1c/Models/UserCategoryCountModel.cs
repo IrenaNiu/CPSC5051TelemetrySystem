2856b87a5d3ab61d7df1d37b19701e9633d7f68d
Mike Koenig <mikekoenig@live.com>
2019-05-29 13:03:25 -0700

    Added UserCategoryCountModel

---
+++ HW1c/Models/UserCategoryCountModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1c.Models
+{
+    public class UserCategoryCountModel
+    {
+        public string Clinic { get; set; }
+        public int UserNumber{ get; set; }
+    }
+}
