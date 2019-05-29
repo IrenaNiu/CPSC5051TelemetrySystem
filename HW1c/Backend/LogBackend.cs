6b87e030d2b8a93e406b25d5031e6cee7a18972c
fanchenjie <fanchenjie@seattleu.edu>
2019-05-29 11:26:13 -0700

    Added column , DeviationRate into accuracy table

--- HW1c/Backend/LogBackend.cs
+++ HW1c/Backend/LogBackend.cs
@@ -17,15 +17,15 @@ namespace HW1c.Backend
             var myData = new LogViewModel();
 
             // TODO:  Populate some Log Data here...
-            myData.LogList.Add(new LogModel { AppVersion = "1",PhoneID = "ABC",RecordedDateTime = DateTime.Now,Location = "Mocc" ,TestValue = "15",LabValue = "20",Accuracy="Y"});
-            myData.LogList.Add(new LogModel { AppVersion = "2", PhoneID = "MNO", RecordedDateTime = DateTime.Parse("01/23/2019"),Location = "Ken", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "3", PhoneID = "ZYX", RecordedDateTime = DateTime.Now.AddDays(-2), Location = "Cana", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "ZYXa", RecordedDateTime = DateTime.Now.AddYears(-1), Location = "Jonw", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "2", PhoneID = "EEE", RecordedDateTime = DateTime.Now, Location = "AA", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "2.3", PhoneID = "MQD", RecordedDateTime = DateTime.Parse("01/29/2019"), Location = "Ben", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "3", PhoneID = "ZX", RecordedDateTime = DateTime.Now.AddDays(-3), Location = "Dana", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "Ya", RecordedDateTime = DateTime.Now.AddYears(-2), Location = "Onw", TestValue = "15", LabValue = "20", Accuracy = "Y" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "Ya", RecordedDateTime = DateTime.Now.AddYears(-2), Location = "Onw", TestValue =  "15", LabValue = "20", Accuracy = "Y" });
+            myData.LogList.Add(new LogModel { AppVersion = "1",PhoneID = "ABC",RecordedDateTime = DateTime.Now,Location = "Mocc" ,TestValue = "15",LabValue = "20",Accuracy="Y", DeviationRate ="5%"});
+            myData.LogList.Add(new LogModel { AppVersion = "2", PhoneID = "MNO", RecordedDateTime = DateTime.Parse("01/23/2019"),Location = "Ken", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3", PhoneID = "ZYX", RecordedDateTime = DateTime.Now.AddDays(-2), Location = "Cana", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "ZYXa", RecordedDateTime = DateTime.Now.AddYears(-1), Location = "Jonw", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "2", PhoneID = "EEE", RecordedDateTime = DateTime.Now, Location = "AA", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "2.3", PhoneID = "MQD", RecordedDateTime = DateTime.Parse("01/29/2019"), Location = "Ben", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3", PhoneID = "ZX", RecordedDateTime = DateTime.Now.AddDays(-3), Location = "Dana", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "Ya", RecordedDateTime = DateTime.Now.AddYears(-2), Location = "Onw", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "Ya", RecordedDateTime = DateTime.Now.AddYears(-2), Location = "Onw", TestValue =  "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
 
             return myData;
         }
