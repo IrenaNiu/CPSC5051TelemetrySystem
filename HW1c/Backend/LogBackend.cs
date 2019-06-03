00e553273d5d0d24c7541e4b9e4fa8b1ba42e7d4
fanchenjie <fanchenjie@seattleu.edu>
2019-06-03 12:50:01 -0700

    Edited the date of fake data table

--- HW1c/Backend/LogBackend.cs
+++ HW1c/Backend/LogBackend.cs
@@ -18,14 +18,14 @@ namespace HW1c.Backend
 
             // TODO:  Populate some Log Data here...
             myData.LogList.Add(new LogModel { AppVersion = "1.0",PhoneID = "AA12CW776",RecordedDateTime = DateTime.Now,Location = "Clinic A" ,TestValue = "15",LabValue = "20",Accuracy="Y", DeviationRate ="5%"});
-            myData.LogList.Add(new LogModel { AppVersion = "2.0", PhoneID = "AA13RF854", RecordedDateTime = DateTime.Parse("01/23/2019"),Location = "Clinic B", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "2.0", PhoneID = "AA13RF854", RecordedDateTime = DateTime.Parse("06/02/2019"),Location = "Clinic B", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
             myData.LogList.Add(new LogModel { AppVersion = "3.0", PhoneID = "AA16TG096", RecordedDateTime = DateTime.Now.AddDays(-2), Location = "Clinic B", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "AA15TK076", RecordedDateTime = DateTime.Now.AddYears(-1), Location = "Clinic D", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
-            myData.LogList.Add(new LogModel { AppVersion = "2.0", PhoneID = "AA16EG025", RecordedDateTime = DateTime.Now, Location = "Clinic C", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
-            myData.LogList.Add(new LogModel { AppVersion = "2.3", PhoneID = "AA03TB093", RecordedDateTime = DateTime.Parse("01/29/2019"), Location = "Clinic B", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.0", PhoneID = "AA56RU937", RecordedDateTime = DateTime.Now.AddDays(-3), Location = "Clinic C", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "AA76BM405", RecordedDateTime = DateTime.Now.AddYears(-2), Location = "Clinic D", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
-            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "AA36TD842", RecordedDateTime = DateTime.Now.AddYears(-2), Location = "Clinic D", TestValue =  "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "AA15TK076", RecordedDateTime = DateTime.Now.AddDays(-5), Location = "Clinic D", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "2.0", PhoneID = "AA16EG025", RecordedDateTime = DateTime.Now.AddDays(-5), Location = "Clinic C", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "2.3", PhoneID = "AA03TB093", RecordedDateTime = DateTime.Parse("05/28/2019"), Location = "Clinic B", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.0", PhoneID = "AA56RU937", RecordedDateTime = DateTime.Now.AddDays(-7), Location = "Clinic C", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "AA76BM405", RecordedDateTime = DateTime.Now.AddDays(-7), Location = "Clinic D", TestValue = "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
+            myData.LogList.Add(new LogModel { AppVersion = "3.3", PhoneID = "AA36TD842", RecordedDateTime = DateTime.Now.AddDays(-8), Location = "Clinic D", TestValue =  "15", LabValue = "20", Accuracy = "Y", DeviationRate = "5%" });
 
             return myData;
         }
